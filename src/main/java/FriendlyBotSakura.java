8d26f4f65cb111679a94f070ab0b77fdda7d624b
tokitobashi1 <suihao8@gmail.com>
2025-08-20 12:08:25 +0800

    Level-3

--- src/main/java/FriendlyBotSakura.java
+++ src/main/java/FriendlyBotSakura.java
@@ -1,19 +1,59 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Task class: Represents a single Task(Encapsulation)
+class Task {
+    private String description;
+    private boolean Done;
+
+    public Task(String description) {
+        this.description = description;
+        this.Done = false;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getDone() {
+        return Done;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean done) {
+        this.Done = done;
+    }
+    public String getStatus() {
+        return (Done ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.Done = true;
+    }
+
+    public void NotDone() {
+        this.Done = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + description;
+    }
+}
+
 public class FriendlyBotSakura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>(); // store tasks in this variable here!
+        ArrayList<Task> tasks = new ArrayList<>(); // store tasks here
 
         System.out.println("____________________________________________________________");
         System.out.println("Hi, I am your friendly bot \uD83C\uDF38Sakura\uD83C\uDF38, feel free to let me know what you need!");
         System.out.println("How can I assist you?");
         System.out.println("____________________________________________________________");
 
-        String input;
         while (true) {
-            input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
@@ -23,13 +63,30 @@ public class FriendlyBotSakura {
             }
             else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    System.out.println(" " + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
             }
+            else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks.get(index).markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" \uD83C\uDF37Nice! I've marked this task as done:");
+                System.out.println("   " + tasks.get(index));
+                System.out.println("____________________________________________________________");
+            }
+            else if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks.get(index).NotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" \uD83C\uDF37I have marked this task as not done:");
+                System.out.println("   " + tasks.get(index));
+                System.out.println("____________________________________________________________");
+            }
             else {
-                tasks.add(input); // add to list
+                tasks.add(new Task(input)); // add to list
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
