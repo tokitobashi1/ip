93e05dc940e16645c599f6b0ae32efa6b63474fa
tokitobashi1 <suihao8@gmail.com>
2025-08-20 11:48:44 +0800

    Level-0

---
+++ src/main/java/FriendlyBotSakura.java
@@ -0,0 +1,9 @@
+public class FriendlyBotSakura {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hi, I am your friendly bot Sakura, feel free to let me know what you need!");
+        System.out.println("How can i assist you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye, I wish you a pleasant day!");
+    }
+}
