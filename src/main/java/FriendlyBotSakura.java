33439c0f7a905547552ae6a42805c8196426c974
tokitobashi1 <suihao8@gmail.com>
2025-08-20 12:03:30 +0800

    Level-2

--- src/main/java/FriendlyBotSakura.java
+++ src/main/java/FriendlyBotSakura.java
@@ -1,27 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class FriendlyBotSakura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hi, I am your friendly bot Sakura, feel free to let me know what you need!");
-        System.out.println("How can i assist you?");
+        ArrayList<String> tasks = new ArrayList<>(); // store tasks in this variable here!
 
+        System.out.println("____________________________________________________________");
+        System.out.println("Hi, I am your friendly bot \uD83C\uDF38Sakura\uD83C\uDF38, feel free to let me know what you need!");
+        System.out.println("How can I assist you?");
+        System.out.println("____________________________________________________________");
 
         String input;
-        while (true){
-            input = sc.nextLine(); // assign it again, so it changes based on the input of the user
+        while (true) {
+            input = sc.nextLine();
 
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" Bye, I wish you a pleasant day!");
+                System.out.println(" Bye, I wish you a pleasant day!\uD83C\uDF37");
+                System.out.println("____________________________________________________________");
                 break;
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
+            else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                tasks.add(input); // add to list
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
+            }
         }
+
+        sc.close();
     }
 }
