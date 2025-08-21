996af64eb1a4a72b458d2ffdeb639ae6a696d8c1
tokitobashi1 <suihao8@gmail.com>
2025-08-21 11:31:19 +0800

    Level-4

--- src/main/java/FriendlyBotSakura.java
+++ src/main/java/FriendlyBotSakura.java
@@ -42,6 +42,50 @@ class Task {
     }
 }
 
+// To_Do class subclass
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+// Deadline subclass
+class Deadline extends Task {
+    private String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+// Event subclass
+class Event extends Task {
+    private String from;
+    private String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
 public class FriendlyBotSakura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -63,30 +107,75 @@ public class FriendlyBotSakura {
             }
             else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" Here are the tasks in your list:");
+                System.out.println(" \uD83C\uDF38Here are the tasks in your list\uD83C\uDF38:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(" " + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
             }
-            else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                tasks.get(index).markAsDone();
-                System.out.println("____________________________________________________________");
-                System.out.println(" \uD83C\uDF37Nice! I've marked this task as done:");
-                System.out.println("   " + tasks.get(index));
-                System.out.println("____________________________________________________________");
+            else if (input.startsWith("mark")) {
+                try {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    tasks.get(index).markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" \uD83C\uDF37Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks.get(index));
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Please reenter a valid task number.");
+                    System.out.println("____________________________________________________________");
+                }
             }
             else if (input.startsWith("unmark")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                tasks.get(index).NotDone();
+                try {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    tasks.get(index).NotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" \uD83C\uDF37I have marked this task as not done:");
+                    System.out.println("   " + tasks.get(index));
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Please reenter a valid task number.");
+                    System.out.println("____________________________________________________________");
+                }
+            }
+            else if (input.startsWith("todo")) {
+                String description = input.substring(5).trim();
+                tasks.add(new ToDo(description));
                 System.out.println("____________________________________________________________");
-                System.out.println(" \uD83C\uDF37I have marked this task as not done:");
-                System.out.println("   " + tasks.get(index));
+                System.out.println(" \uD83C\uDF37I have added this task:");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" \uD83C\uDF37You now have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }
+            else if (input.startsWith("deadline")) {
+                String The_Rest = input.substring(9).trim(); // after "deadline "
+                String[] parts = The_Rest.split(" /by ", 2);
+                tasks.add(new Deadline(parts[0].trim(), parts[1].trim()));
+                System.out.println("____________________________________________________________");
+                System.out.println("  \uD83C\uDF37I have added this task:");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" \uD83C\uDF37You now have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }
+            else if (input.startsWith("event")) {
+                String The_Rest1 = input.substring(6).trim();
+                int fromIndex = The_Rest1.indexOf(" /from ");
+                int toIndex = The_Rest1.indexOf(" /to ");
+                String description1 = The_Rest1.substring(0, fromIndex).trim();
+                String from = The_Rest1.substring(fromIndex + 7, toIndex).trim();
+                String to = The_Rest1.substring(toIndex + 5).trim();
+                tasks.add(new Event(description1, from, to));
+                System.out.println("____________________________________________________________");
+                System.out.println("  \uD83C\uDF37I have added this task:");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" \uD83C\uDF37You now have " + tasks.size() + " tasks in the list.");
                 System.out.println("____________________________________________________________");
             }
             else {
-                tasks.add(new Task(input)); // add to list
+                tasks.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
